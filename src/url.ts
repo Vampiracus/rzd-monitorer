2849767c20e9fa1d5f688faafa8290c3b9890c7a
Max Kudrin <kudrin.ma2012@ya.ru>
2024-11-25 12:44:41 +0300

    change config

--- src/url.ts
+++ src/url.ts
@@ -28,5 +28,20 @@ export const URLS = [
     url: "https://ticket.rzd.ru/api/v1/railway-service/prices/train-pricing?service_provider=B2B_RZD&origin=2064060&destination=2000000&departureDate=2025-01-06T00:00:00&timeFrom=0&timeTo=24&carGrouping=DontGroup&getByLocalTime=true&specialPlacesDemand=StandardPlacesAndForDisabledPersons&carIssuingType=All&getTrainsFromSchedule=true",
     name: "ПТГ-МСК-06.01",
     link: "https://ticket.rzd.ru/searchresults/v/1/5a13b9e6340c745ca1e7c695/5a323c29340c7441a0a556bb/2025-01-06"
+  },
+  {
+    url: "https://ticket.rzd.ru/api/v1/railway-service/prices/train-pricing?service_provider=B2B_RZD&origin=2000000&destination=2064055&departureDate=2024-12-28T00:00:00&timeFrom=0&timeTo=24&carGrouping=DontGroup&getByLocalTime=true&specialPlacesDemand=StandardPlacesAndForDisabledPersons&carIssuingType=All&getTrainsFromSchedule=true",
+    name: "МСК-НЛ-28.01",
+    link: "https://ticket.rzd.ru/searchresults/v/1/5a323c29340c7441a0a556bb/5a13b96b340c745ca1e7af87/2024-12-28"
+  },
+  {
+    url: "https://ticket.rzd.ru/api/v1/railway-service/prices/train-pricing?service_provider=B2B_RZD&origin=2000000&destination=2064170&departureDate=2024-12-28T00:00:00&timeFrom=0&timeTo=24&carGrouping=DontGroup&getByLocalTime=true&specialPlacesDemand=StandardPlacesAndForDisabledPersons&carIssuingType=All&getTrainsFromSchedule=true",
+    name: "МСК-МВ-28.01",
+    link: "https://ticket.rzd.ru/searchresults/v/1/5a323c29340c7441a0a556bb/5a13ba94340c745ca1e7ee67/2024-12-28"
+  },
+  {
+    url: "https://ticket.rzd.ru/api/v1/railway-service/prices/train-pricing?service_provider=B2B_RZD&origin=2000000&destination=2064060&departureDate=2024-12-28T00:00:00&timeFrom=0&timeTo=24&carGrouping=DontGroup&getByLocalTime=true&specialPlacesDemand=StandardPlacesAndForDisabledPersons&carIssuingType=All&getTrainsFromSchedule=true",
+    name: "МСК-ПТГ-28.01",
+    link: "https://ticket.rzd.ru/searchresults/v/1/5a323c29340c7441a0a556bb/5a13b9e6340c745ca1e7c695/2024-12-28"
   }
 ]
